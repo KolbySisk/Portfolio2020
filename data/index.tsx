f017fe60e6d9ecf7f7e979041cfb735fd362742b
unknown <kolby.sisk@gmail.com>
2021-08-27 16:41:48 -0500

    Update intro

--- data/index.tsx
+++ data/index.tsx
@@ -10,11 +10,11 @@ export interface Data {
 }
 
 const work: any = Object.values(Work);
-const uniqueTags: string[] = Array.from(new Set([].concat(...work.map(w => w.tags))));
+const uniqueTags: string[] = Array.from(new Set([].concat(...work.map((w) => w.tags))));
 
 const data: Data = {
   intro:
-    'Builder of software with a passion for learning. I specialize in web development and user experience design, however I enjoy the entire software development process. My favorite tech stack is currently TS Node & Next.js. I spend my days working with a talented team building a design system at NCR.',
+    'Builder of software with a passion for learning. I specialize in application development and user experience design, however I enjoy the entire software development process. My favorite tech stack is currently TS Node & Next.js. I spend my days working with a talented team building next-gen pharmacy software at Walmart.',
   work: work,
   tags: uniqueTags,
   skills: AllSkills,
